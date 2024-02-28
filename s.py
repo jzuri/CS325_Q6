e437380c927e731c073e4b42162a3ac657b12903
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-28 17:49:39 -0600

    Update s.py

--- s.py
+++ s.py
@@ -1,1 +1,9 @@
+# Write a program on how you would refactor the Order class to follow the Single Responsibility Principle
+
+class Order:
+  def __init__(self, customer_info, items, shipping_address):
+    self.customer_info = customer_info
+    self.items = items
+    self.shipping_address = shipping_address
+    
 
